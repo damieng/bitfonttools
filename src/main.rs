0191543d2a9656b8ec83d028ad9774ad234bb593
Damien Guard <damieng@gmail.com>
2021-02-03 01:48:29 +0000

    Make space same width as M

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use structopt::StructOpt;
 use std::process::exit;
-use std::collections::hash_map::Entry;
 
 fn main() {
+    println!("make-bdf-proportional v0.0.1");
+
     let args = Cli::from_args();
 
-    match bdf::open(&args.inFileName).as_mut()
+    let mut file = bdf::open(&args.in_filename);
+
+    match file.as_mut()
     {
         Ok(font) => {
             let glyphs = font.glyphs_mut();
+            let mut space_width = 0;
             for gl in glyphs {
                 let glyph = gl.1;
                 let mut width = glyph.width();
@@ -22,14 +26,33 @@ fn main() {
                     }
                 }
 
-                println!("{}", width);
+                if gl.0 == &'M' {
+                    space_width = width;
+                }
+
                 glyph.set_device_width(Some((width, 0)));
             }
 
-            bdf::save(&args.outFileName, &font);
+            let space = font.glyphs_mut().get_mut(&' ');
+            match space {
+                Some(space_glyph) => {
+                    space_glyph.set_device_width(Some((space_width, 0)));
+                }
+                None => {}
+            }
+
+            match bdf::save(&args.out_filename, &font) {
+                Ok(_) => {
+                    println!("Proportional version created.");
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    exit(1);
+                }
+            }
         }
         Err(e) => {
-            eprintln!("{}", &*e);
+            eprintln!("{}", e);
             exit(1);
         }
     }
@@ -40,8 +63,8 @@ fn main() {
 struct Cli {
     /// The path to the file to read
     #[structopt(parse(from_os_str))]
-    inFileName: std::path::PathBuf,
+    in_filename: std::path::PathBuf,
 
     #[structopt(parse(from_os_str))]
-    outFileName: std::path::PathBuf,
+    out_filename: std::path::PathBuf,
 }
